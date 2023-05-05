82fe0f6996c7bf49fa580f7c31844dc8e85630f4
OnigbodeNathan <131052547+OnigbodeNathan@users.noreply.github.com>
2023-05-05 14:09:59 +0100

    Update Energy.java

--- Energy.java
+++ Energy.java
@@ -3,7 +3,7 @@ public class Energy extends Physics {
     double kineticEnergy(float mass, float velocity) {
         return multiply(multiply(0.5 ,mass),pow2(velocity));
     }
-    double potentialEnergy(int mass, float gravity, int height){
+    double potentialEnergy(int mass, getgravity, int height){
         return multiply(multiply(mass, gravity), height));
     }
     double velocity(float distance, float time){
